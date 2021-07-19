9b2c9442ec06f0d467827d5e75aab53625d4f764
Algure <ajirie98@gmail.com>
2021-07-19 15:37:02 +0100

    created initial models

--- dialerapp/models.py
+++ dialerapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=30)
+    company_id = models.CharField(max_length=50)
+    company_name = models.CharField(max_length = 100)
+    session_duration = models.CharField(max_length=50)
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    playlist = models.TextField()
+    
