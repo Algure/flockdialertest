72526dd757ac048ecbeaef52ae15f4d626c532c3
Algure <ajirie98@gmail.com>
2021-07-19 22:38:15 +0100

    api works

--- autodialer/urls.py
+++ autodialer/urls.py
@@ -6,7 +6,7 @@ import dialerapp.api_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/user/create/', dialerapp.api_views.UserCreate.as_view()),
-    # path('api/user/query/', dialerapp.api_views.UserList.as_view()),
+    path('api/user/create/', dialerapp.api_views.UserCreate.as_view(),),
+    path('api/user/query/', dialerapp.api_views.UserList.as_view()),
 
 ]
--- dialerapp/api_views.py
+++ dialerapp/api_views.py
@@ -5,37 +5,36 @@ from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
-from .models import User
-from .models import Company
-from .serializers import UserSerializer
+from dialerapp.models import User
+from dialerapp.models import Company
+from dialerapp.serializers import UserSerializer
 
-# class DataPagination(LimitOffsetPagination):
-#     default_limit = 10
-#     max_limit = 100
+class DataPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
-# class UserList(ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     filter_backends = (DjangoFilterBackend, SearchFilter)
-#     filter_fields = ('id', 'company_id')
-#     search_fields = ('name', 'company_name')
-#     pagination_class = DataPagination
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filter_fields = ('id', 'company_id')
+    search_fields = ('name', 'company_name')
+    pagination_class = DataPagination
 
-# class UserCreate(CreateAPIView):
-#     # queryset = User.objects.all()
-#     serializer_class = UserSerializer
+class UserCreate(CreateAPIView):
+    serializer_class = UserSerializer
 
-#     # def create(self, request, *args, **kwargs):
-#     #     try:
-#     #         price = request.data.get('price')
-#     #         if price is not None and float(price) <= 0.0:
-#     #             raise ValidationError({ 'price': 'Must be above $0.00' })
-#     #     except ValueError:
-#     #         raise ValidationError({ 'price': 'A valid number is required' })
-#     #     response = super().create(request, *args, **kwargs)
+    # def create(self, request, *args, **kwargs):
+    #     try:
+    #         price = request.data.get('price')
+    #         if price is not None and float(price) <= 0.0:
+    #             raise ValidationError({ 'price': 'Must be above $0.00' })
+    #     except ValueError:
+    #         raise ValidationError({ 'price': 'A valid number is required' })
+    #     response = super().create(request, *args, **kwargs)
 
-# class UserRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-#     queryset = User.objects.all()
-#     lookup_field = 'id'
-#     serializer_class = UserSerializer
+class UserRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    lookup_field = 'id'
+    serializer_class = UserSerializer
 
